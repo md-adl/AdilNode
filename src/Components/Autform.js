26e4540c653ec83a3437f891bf6ed25e02c1e0e7
Md Adil <mdaadil88040@gmail.com>
2024-03-12 15:47:26 +0530

    update

--- src/Components/Autform.js
+++ src/Components/Autform.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Dashboard from './Dashboard';
 
 const AuthForm = ({ onAuthSuccess }) => {
   const [email, setEmail] = useState('');
@@ -10,6 +10,7 @@ const AuthForm = ({ onAuthSuccess }) => {
   const [username, setUsername] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [isLoginPage, setIsLoginPage] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleSignup = async () => {
     try {
@@ -21,6 +22,7 @@ const AuthForm = ({ onAuthSuccess }) => {
       });
       console.log(response);
       onAuthSuccess();
+      setIsLoggedIn(true);
     } catch (error) {
       console.error('Error signing up:', error);
     }
@@ -28,12 +30,14 @@ const AuthForm = ({ onAuthSuccess }) => {
 
   const handleLogin = async () => {
     try {
-        const response = await axios.post('http://localhost:5001/api/v1/user/loginUser', { 
-            email, password 
-          });
-          
+      const response = await axios.post('http://localhost:5001/api/v1/user/loginUser', {
+        email,
+        password
+      });
+
       console.log(response);
       onAuthSuccess();
+      setIsLoggedIn(true);
     } catch (error) {
       console.error('Error logging in:', error);
     }
@@ -47,46 +51,49 @@ const AuthForm = ({ onAuthSuccess }) => {
     <div className="container mt-5">
       <div className="row">
         <div className="col-md-6">
-          {isLoginPage ? (
-            <>
-              <h2>Login</h2>
-              <div className="mb-3">
-                <label htmlFor="loginEmail" className="form-label">Email</label>
-                <input type="email" className="form-control" id="loginEmail" value={email} onChange={(e) => setEmail(e.target.value)} />
-              </div>
-              <div className="mb-3">
-                <label htmlFor="loginPassword" className="form-label">Password</label>
-                <input type="password" className="form-control" id="loginPassword" value={password} onChange={(e) => setPassword(e.target.value)} />
-              </div>
-              <button className="btn btn-success" onClick={handleLogin}>Login</button>
-              <p className="mt-3">Don't have an account? <span className="text-primary" onClick={switchForm}>Switch to  SignUp</span></p>
-            </>
+          {isLoggedIn ? (
+            <Dashboard username = {username} email={email}/>
           ) : (
             <>
-              
-              <h2>Signup</h2>
-              <div className="mb-3">
-                <label htmlFor="signupEmail" className="form-label">Email</label>
-                <input type="email" className="form-control" id="signupEmail" value={email} onChange={(e) => setEmail(e.target.value)} />
-              </div>
-              <div className="mb-3">
-                <label htmlFor="signupPassword" className="form-label">Password</label>
-                <input type="password" className="form-control" id="signupPassword" value={password} onChange={(e) => setPassword(e.target.value)} />
-              </div>
-              <div className="mb-3">
-                <label htmlFor="signupUsername" className="form-label">Username</label>
-                <input type="text" className="form-control" id="signupUsername" value={username} onChange={(e) => setUsername(e.target.value)} />
-              </div>
-              <div className="mb-3">
-                <label htmlFor="signupPhoneNumber" className="form-label">Phone Number</label>
-                <input type="text" className="form-control" id="signupPhoneNumber" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
-              </div>
-              <button className="btn btn-primary" onClick={handleSignup}>Signup</button>
-              <p className="mt-3">Already Registered <span className="text-primary" onClick={switchForm}>Switch to Login</span></p>
+              {isLoginPage ? (
+                <>
+                  <h2>Login</h2>
+                  <div className="mb-3">
+                    <label htmlFor="loginEmail" className="form-label">Email</label>
+                    <input type="email" className="form-control" id="loginEmail" value={email} onChange={(e) => setEmail(e.target.value)} />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="loginPassword" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="loginPassword" value={password} onChange={(e) => setPassword(e.target.value)} />
+                  </div>
+                  <button className="btn btn-success" onClick={handleLogin}>Login</button>
+                  <p className="mt-3">Don't have an account? <span className="text-primary" onClick={switchForm}>Switch to SignUp</span></p>
+                </>
+              ) : (
+                <>
+                  <h2>Signup</h2>
+                  <div className="mb-3">
+                    <label htmlFor="signupEmail" className="form-label">Email</label>
+                    <input type="email" className="form-control" id="signupEmail" value={email} onChange={(e) => setEmail(e.target.value)} />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="signupPassword" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="signupPassword" value={password} onChange={(e) => setPassword(e.target.value)} />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="signupUsername" className="form-label">Username</label>
+                    <input type="text" className="form-control" id="signupUsername" value={username} onChange={(e) => setUsername(e.target.value)} />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="signupPhoneNumber" className="form-label">Phone Number</label>
+                    <input type="text" className="form-control" id="signupPhoneNumber" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+                  </div>
+                  <button className="btn btn-primary" onClick={handleSignup}>Signup</button>
+                  <p className="mt-3">Already Registered <span className="text-primary" onClick={switchForm}>Switch to Login</span></p>
+                </>
+              )}
             </>
-            
           )}
-          
         </div>
       </div>
     </div>
--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,11 +1,12 @@
 // Dashboard.js
 import React from 'react';
 
-const Dashboard = () => {
+const Dashboard = ({username, email}) => {
   return (
     <div>
       <h2>Welcome to the Dashboard!</h2>
       {/* Add your dashboard content here */}
+      <p>Greeting {username} and Your email {email}</p>
     </div>
   );
 };
